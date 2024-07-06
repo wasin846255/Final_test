d945d427f1b7ceb0d72ca9630079a15abd0a84d9
Wasin <wasin1026@gmail.com>
2024-07-06 23:04:22 +0700

    Q5

--- question-5.js
+++ question-5.js
@@ -1,7 +1,12 @@
 // Question 5: Calculating Total Price in User's Cart
 
-function calculateTotalPrice() {
+function calculateTotalPrice(cart) {
   // เริ่มเขียนโค้ดตรงนี้
+  let total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    total += cart[i].price * cart[i].quantity;
+  }
+  return total;
 }
 
 // ตัวอย่างการใช้งาน
