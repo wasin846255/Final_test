1c6c8b02268ee33a3599b8e1cac2c22c62864cb4
Wasin <wasin1026@gmail.com>
2024-07-06 23:42:45 +0700

    Q8

--- question-8.js
+++ question-8.js
@@ -1,3 +1,9 @@
 // Question #8: Fetching User List from Server
 
 // เริ่มเขียนโค้ดตรงนี้
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((users) => {
+    const newUsers = users.map((user) => user.name);
+    console.log(newUsers);
+  });
