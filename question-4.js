bf02c0e1f5064cba247e9e9747db309df4d59c50
Wasin <wasin1026@gmail.com>
2024-07-06 22:58:48 +0700

    Q4

--- question-4.js
+++ question-4.js
@@ -1,7 +1,14 @@
 // Question 4: Shipping Cost Calculator
 
-function calculateShippingCost() {
+function calculateShippingCost(order) {
   // เริ่มเขียนโค้ดที่นี่
+  if (order > 4000) {
+    return "Shipping is free.";
+  } else if (order > 2000) {
+    return "Shipping cost is 250 Baht.";
+  } else {
+    return "Shipping cost is 500 Baht.";
+  }
 }
 
 // ตัวอย่างการใช้งาน
